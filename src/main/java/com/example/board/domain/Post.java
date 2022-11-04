c34b2ebb660dbe1c35a3ba69a2688c7644ab1723
Gahyun Lee <ab3380@naver.com>
2022-11-04 22:38:55 +0900

    udate 수정

--- src/main/java/com/example/board/domain/Post.java
+++ src/main/java/com/example/board/domain/Post.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Getter
 @Entity
 @NoArgsConstructor
-public class Post {
+public class Post extends BaseTimeEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
